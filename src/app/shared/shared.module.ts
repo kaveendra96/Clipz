948bfc7e68a2f81eabee5ddd176cf73bf69dc96c
kaveendra96 <kaveendra96@gmail.com>
2022-03-23 08:42:35 +0530

    Login and registraion forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 //import { ModalService } from '../services/modal.service';
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent
   ]
   // providers:[
   //   ModalService  // inject service into module lavel
